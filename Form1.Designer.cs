48121f474c36ae8b505de4711e8fb786a1f44334
Phong Vo <vhphong>
2020-07-08 23:21:12 -0400

    first commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,317 @@
+﻿namespace StudentManagerCSharpXamppMySQL
+{
+	partial class FormUI
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.label1 = new System.Windows.Forms.Label();
+			this.label2 = new System.Windows.Forms.Label();
+			this.label3 = new System.Windows.Forms.Label();
+			this.txtSID = new System.Windows.Forms.TextBox();
+			this.label4 = new System.Windows.Forms.Label();
+			this.txtLName = new System.Windows.Forms.TextBox();
+			this.label5 = new System.Windows.Forms.Label();
+			this.txtSSN = new System.Windows.Forms.TextBox();
+			this.label6 = new System.Windows.Forms.Label();
+			this.txtFName = new System.Windows.Forms.TextBox();
+			this.label7 = new System.Windows.Forms.Label();
+			this.txtDOB = new System.Windows.Forms.TextBox();
+			this.label8 = new System.Windows.Forms.Label();
+			this.txtRace = new System.Windows.Forms.TextBox();
+			this.label9 = new System.Windows.Forms.Label();
+			this.txtGender = new System.Windows.Forms.TextBox();
+			this.label10 = new System.Windows.Forms.Label();
+			this.txtAvatar = new System.Windows.Forms.TextBox();
+			this.label11 = new System.Windows.Forms.Label();
+			this.txtSubmissions = new System.Windows.Forms.TextBox();
+			this.btnInsert = new System.Windows.Forms.Button();
+			this.btnViewRecords = new System.Windows.Forms.Button();
+			this.btnExit = new System.Windows.Forms.Button();
+			this.SuspendLayout();
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label1.Location = new System.Drawing.Point(258, 9);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(305, 42);
+			this.label1.TabIndex = 0;
+			this.label1.Text = "Student Manager";
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label2.Location = new System.Drawing.Point(188, 68);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(434, 32);
+			this.label2.TabIndex = 0;
+			this.label2.Text = "Student \'s Information Entry Form";
+			// 
+			// label3
+			// 
+			this.label3.AutoSize = true;
+			this.label3.Location = new System.Drawing.Point(149, 135);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(39, 24);
+			this.label3.TabIndex = 1;
+			this.label3.Text = "SID";
+			// 
+			// txtSID
+			// 
+			this.txtSID.Location = new System.Drawing.Point(291, 135);
+			this.txtSID.Name = "txtSID";
+			this.txtSID.Size = new System.Drawing.Size(468, 28);
+			this.txtSID.TabIndex = 0;
+			// 
+			// label4
+			// 
+			this.label4.AutoSize = true;
+			this.label4.Location = new System.Drawing.Point(149, 203);
+			this.label4.Name = "label4";
+			this.label4.Size = new System.Drawing.Size(99, 24);
+			this.label4.TabIndex = 1;
+			this.label4.Text = "Last Name\t";
+			// 
+			// txtLName
+			// 
+			this.txtLName.Location = new System.Drawing.Point(291, 203);
+			this.txtLName.Name = "txtLName";
+			this.txtLName.Size = new System.Drawing.Size(468, 28);
+			this.txtLName.TabIndex = 2;
+			// 
+			// label5
+			// 
+			this.label5.AutoSize = true;
+			this.label5.Location = new System.Drawing.Point(149, 237);
+			this.label5.Name = "label5";
+			this.label5.Size = new System.Drawing.Size(135, 24);
+			this.label5.TabIndex = 1;
+			this.label5.Text = "SSN (Optional)\t";
+			// 
+			// txtSSN
+			// 
+			this.txtSSN.Location = new System.Drawing.Point(291, 237);
+			this.txtSSN.Name = "txtSSN";
+			this.txtSSN.Size = new System.Drawing.Size(468, 28);
+			this.txtSSN.TabIndex = 3;
+			// 
+			// label6
+			// 
+			this.label6.AutoSize = true;
+			this.label6.Location = new System.Drawing.Point(149, 169);
+			this.label6.Name = "label6";
+			this.label6.Size = new System.Drawing.Size(101, 24);
+			this.label6.TabIndex = 1;
+			this.label6.Text = "First Name\t";
+			// 
+			// txtFName
+			// 
+			this.txtFName.Location = new System.Drawing.Point(291, 169);
+			this.txtFName.Name = "txtFName";
+			this.txtFName.Size = new System.Drawing.Size(468, 28);
+			this.txtFName.TabIndex = 1;
+			// 
+			// label7
+			// 
+			this.label7.AutoSize = true;
+			this.label7.Location = new System.Drawing.Point(149, 271);
+			this.label7.Name = "label7";
+			this.label7.Size = new System.Drawing.Size(109, 24);
+			this.label7.TabIndex = 1;
+			this.label7.Text = "Date of birth\t";
+			// 
+			// txtDOB
+			// 
+			this.txtDOB.Location = new System.Drawing.Point(291, 271);
+			this.txtDOB.Name = "txtDOB";
+			this.txtDOB.Size = new System.Drawing.Size(468, 28);
+			this.txtDOB.TabIndex = 4;
+			// 
+			// label8
+			// 
+			this.label8.AutoSize = true;
+			this.label8.Location = new System.Drawing.Point(149, 339);
+			this.label8.Name = "label8";
+			this.label8.Size = new System.Drawing.Size(54, 24);
+			this.label8.TabIndex = 1;
+			this.label8.Text = "Race";
+			// 
+			// txtRace
+			// 
+			this.txtRace.Location = new System.Drawing.Point(291, 339);
+			this.txtRace.Name = "txtRace";
+			this.txtRace.Size = new System.Drawing.Size(468, 28);
+			this.txtRace.TabIndex = 6;
+			// 
+			// label9
+			// 
+			this.label9.AutoSize = true;
+			this.label9.Location = new System.Drawing.Point(149, 305);
+			this.label9.Name = "label9";
+			this.label9.Size = new System.Drawing.Size(74, 24);
+			this.label9.TabIndex = 1;
+			this.label9.Text = "Gender\t";
+			// 
+			// txtGender
+			// 
+			this.txtGender.Location = new System.Drawing.Point(291, 305);
+			this.txtGender.Name = "txtGender";
+			this.txtGender.Size = new System.Drawing.Size(468, 28);
+			this.txtGender.TabIndex = 5;
+			// 
+			// label10
+			// 
+			this.label10.AutoSize = true;
+			this.label10.Location = new System.Drawing.Point(149, 373);
+			this.label10.Name = "label10";
+			this.label10.Size = new System.Drawing.Size(62, 24);
+			this.label10.TabIndex = 1;
+			this.label10.Text = "Avatar";
+			// 
+			// txtAvatar
+			// 
+			this.txtAvatar.Location = new System.Drawing.Point(291, 373);
+			this.txtAvatar.Name = "txtAvatar";
+			this.txtAvatar.Size = new System.Drawing.Size(468, 28);
+			this.txtAvatar.TabIndex = 7;
+			// 
+			// label11
+			// 
+			this.label11.AutoSize = true;
+			this.label11.Location = new System.Drawing.Point(149, 407);
+			this.label11.Name = "label11";
+			this.label11.Size = new System.Drawing.Size(117, 24);
+			this.label11.TabIndex = 1;
+			this.label11.Text = "Submissions";
+			// 
+			// txtSubmissions
+			// 
+			this.txtSubmissions.Location = new System.Drawing.Point(291, 407);
+			this.txtSubmissions.Name = "txtSubmissions";
+			this.txtSubmissions.Size = new System.Drawing.Size(468, 28);
+			this.txtSubmissions.TabIndex = 8;
+			// 
+			// btnInsert
+			// 
+			this.btnInsert.Location = new System.Drawing.Point(153, 462);
+			this.btnInsert.Name = "btnInsert";
+			this.btnInsert.Size = new System.Drawing.Size(180, 60);
+			this.btnInsert.TabIndex = 9;
+			this.btnInsert.Text = "INSERT";
+			this.btnInsert.UseVisualStyleBackColor = true;
+			this.btnInsert.Click += new System.EventHandler(this.btnInsert_Click);
+			// 
+			// btnViewRecords
+			// 
+			this.btnViewRecords.Location = new System.Drawing.Point(366, 462);
+			this.btnViewRecords.Name = "btnViewRecords";
+			this.btnViewRecords.Size = new System.Drawing.Size(180, 60);
+			this.btnViewRecords.TabIndex = 10;
+			this.btnViewRecords.Text = "VIEW RECORDS";
+			this.btnViewRecords.UseVisualStyleBackColor = true;
+			this.btnViewRecords.Click += new System.EventHandler(this.btnViewRecords_Click);
+			// 
+			// btnExit
+			// 
+			this.btnExit.Location = new System.Drawing.Point(579, 462);
+			this.btnExit.Name = "btnExit";
+			this.btnExit.Size = new System.Drawing.Size(180, 60);
+			this.btnExit.TabIndex = 10;
+			this.btnExit.Text = "EXIT";
+			this.btnExit.UseVisualStyleBackColor = true;
+			this.btnExit.Click += new System.EventHandler(this.btnViewRecords_Click);
+			// 
+			// FormUI
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 22F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(882, 556);
+			this.Controls.Add(this.btnExit);
+			this.Controls.Add(this.btnViewRecords);
+			this.Controls.Add(this.btnInsert);
+			this.Controls.Add(this.txtSubmissions);
+			this.Controls.Add(this.txtAvatar);
+			this.Controls.Add(this.label11);
+			this.Controls.Add(this.txtSSN);
+			this.Controls.Add(this.label10);
+			this.Controls.Add(this.label5);
+			this.Controls.Add(this.txtGender);
+			this.Controls.Add(this.txtFName);
+			this.Controls.Add(this.label9);
+			this.Controls.Add(this.label6);
+			this.Controls.Add(this.txtRace);
+			this.Controls.Add(this.txtLName);
+			this.Controls.Add(this.label8);
+			this.Controls.Add(this.label4);
+			this.Controls.Add(this.txtDOB);
+			this.Controls.Add(this.label7);
+			this.Controls.Add(this.txtSID);
+			this.Controls.Add(this.label3);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.label1);
+			this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+			this.Margin = new System.Windows.Forms.Padding(5);
+			this.Name = "FormUI";
+			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+			this.Text = "Data Entry Main Form";
+			this.Load += new System.EventHandler(this.FormUI_Load);
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.Label label3;
+		private System.Windows.Forms.TextBox txtSID;
+		private System.Windows.Forms.Label label4;
+		private System.Windows.Forms.TextBox txtLName;
+		private System.Windows.Forms.Label label5;
+		private System.Windows.Forms.TextBox txtSSN;
+		private System.Windows.Forms.Label label6;
+		private System.Windows.Forms.TextBox txtFName;
+		private System.Windows.Forms.Label label7;
+		private System.Windows.Forms.TextBox txtDOB;
+		private System.Windows.Forms.Label label8;
+		private System.Windows.Forms.TextBox txtRace;
+		private System.Windows.Forms.Label label9;
+		private System.Windows.Forms.TextBox txtGender;
+		private System.Windows.Forms.Label label10;
+		private System.Windows.Forms.TextBox txtAvatar;
+		private System.Windows.Forms.Label label11;
+		private System.Windows.Forms.TextBox txtSubmissions;
+		private System.Windows.Forms.Button btnInsert;
+		private System.Windows.Forms.Button btnViewRecords;
+		private System.Windows.Forms.Button btnExit;
+	}
+}
+
